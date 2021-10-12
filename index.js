0802ef15f6ef815339d0259594e87808c826a951
DrewMcD25 <drewmcd25@yahoo.com>
2021-10-11 19:26:17 -0500

    added express server/routes

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const server = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = 3000
+const client = require ('./db/client')
+client.connect()
+
+const apiRouter = require('./api')
+
+
+
+server.use(cors())
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use((error, req, res, next) => {
+    console.log('SETTING THE STATUS');
+    res.status(500);
+    res.send(error);
+});
+
+
+
+
+server.use('/api', apiRouter);
+
+server.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`);
+});
