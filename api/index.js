222ad804b01ad216fbd3d556dfc8dd4abe8b698f
DrewMcD25 <drewmcd25@yahoo.com>
2021-10-16 09:10:23 -0500

    some routers

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const apiRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const SECRET = require('./secret')
+
+
+
+const usersRouter = require('./users')
+apiRouter.use(async (req, res, next) => {
+    if(req.header('Authorization')) {
+        const authHeader = req.header('Authorization');
+        if (!authHeader) {
+            throw new Error('NO TOKEN');
+        }
+        const token = authHeader.split('Bearer ')[1];
+        const { id } = jwt.verify(token, SECRET);
+        console.log('HERES YOUR ID', id);
+        if(!id) {
+            throw new Error('Invalid token!');
+        }
+        req.user = await getUserById(id);
+        console.log(req.user);
+        next()
+    } else {
+        next();
+    }
+});
+apiRouter.get('/health',(req, res, next)=>{
+    res.send({message:'something'});
+});
+
+
+apiRouter.use('/users', usersRouter);
+
+module.exports = apiRouter
---
+++ api/users.js
@@ -0,0 +1,40 @@
+const usersRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('./secret');
+const {getUserByUsername, createUser, getPublicRoutinesByUser} = require('../db')
+
+usersRouter.post('/register', async (req, res, next) => {
+    const {username, password} = req.body;
+
+    try {
+        console.log('CREATING THE USER');
+        const user = await createUser({username, password});
+        if(req.body.password.length < 8) {
+            res.status(401);
+            next({message: "Entered password is too short"});
+        }
+        console.log("User registered", + JSON.stringify(user));
+        res.send({user});
+    } catch (error) {
+        console.log('THERE WAS AN ERROR');
+        next(error);
+    }
+});
+
+usersRouter.post('/login', async (req, res, next) => {
+    try {
+        const user = await getUserByUsername(req.body.username);
+        console.log(user);
+        const token = jwt.sign({
+            id: user.id,
+            username: user.username
+        }, JWT_SECRET);
+        console.log(token);
+        res.send({token});
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = usersRouter;
